29665aa4d993b2d1ab7665aa3bdddac6a11ef3b9
CI\EBrady <ebrady@us.ci.org>
2023-11-03 14:46:37 -0700

    Add metadata

--- sentiment_results.py
+++ sentiment_results.py
@@ -4,22 +4,22 @@ from pydantic import BaseModel, BaseSettings
 
 
 class Settings(BaseSettings):
-    app_name: str = ""
-    version: str = ""
+    app_name: str = "tweet-sentiment"
+    version: str = "0.0.1"
     description: str = """"""
     contact: Dict = {
-        "name": "",
-        "url": "",
+        "name": "Sentiment Analysis of Tweets",
+        "url": "https://github.com/elise-ip/tweet-sentiment",
         "email": "elise.mo.brady@gmail.com",
     }
     license_info: Dict = {
-        "name": "CDC Source",
-        "url": "https://www.cdc.gov/growthcharts/percentile_data_files.htm",
+        "name": "Kaggle Twitter Sentiment Analysis Dataset",
+        "url": "https://www.kaggle.com/datasets/jp797498e/twitter-entity-sentiment-analysis",
     }
    
     TWITTER_TRAIN: str = r"data\twitter_training.csv"
     TWITTER_VALIDATION: str = r"data\twitter_validation.csv"
 
-    SENTIMENT_MODEL_FILE: str = "models/lid.176.bin"
+    SENTIMENT_MODEL_FILE: str = "models\linear_svm_v0.pkl"
 
 settings = Settings()
