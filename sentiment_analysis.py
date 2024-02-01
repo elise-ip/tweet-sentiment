6cc0b68352eb6d54cdea8a6070d8339d3730d65b
CI\EBrady <ebrady@us.ci.org>
2024-02-01 12:05:33 -0800

    Update reqs

--- sentiment_analysis.py
+++ sentiment_analysis.py
@@ -3,9 +3,9 @@ import pickle
 class PredictSentiment:
     """"""
     def __init__(self, svm_model, vectorizer):
-            self.svm_model = pickle.load(open(svm_model, "rb"))
-            self.vectorizer = pickle.load(open(vectorizer, "rb"))
-            self.tweet_vector = None
+        self.svm_model = pickle.load(open(svm_model, "rb"))
+        self.vectorizer = pickle.load(open(vectorizer, "rb"))
+        self.tweet_vector = None
 
     def predict(self, tweet):
         self.tweet_vector = self.vectorizer.transform(tweet)
